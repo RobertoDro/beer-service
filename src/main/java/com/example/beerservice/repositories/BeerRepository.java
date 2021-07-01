6f88bddc61f007f649691442c63c7135d86b8022
RobertoDro <37771467+RobertoDro@users.noreply.github.com>
2021-07-01 20:40:30 +0300

    adding beer repository

---
+++ src/main/java/com/example/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,13 @@
+package com.example.beerservice.repositories;
+
+import com.example.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+
+}
